1b4f3359d58d72447dac71fdf3ea48c56e7d9300
teddyxiong53 <1073167306@qq.com>
2018-11-26 20:58:59 +0800

    开始自己写叮当音箱代码。

---
+++ mydingdang/dingdang.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import argparse
+import os, sys
+import os.path
+import logging
+
+parser = argparse.ArgumentParser(description="this is my dingdang speaker")
+parser.add_argument("--local", action="store_true", help="use stdin to input rather than mic ")
+parser.add_argument("--diagnose", action="store_true", help="to diagnose the network")
+parser.add_argument("--verbose", action="store_true", help="verbose mode")
+
+
+args = parser.parse_args()
+
+#print args.local
+
+class Dingdang(object):
+	def __init__(self):
+		pass
+if __name__ == '__main__':
+	logging.basicConfig(
+		format="%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s: %(message)s",
+		level=logging.INFO
+	)
+	logger = logging.getLogger()
+	logger.setLevel(logging.INFO)
+	try :
+		app = Dingdang()
+	except Exception:
+		logger.exception("error occured")
+		sys.exit(1)
+		
+	try:
+		app.run()
+	except KeyboardInterrupt:
+		logger.info("dingdang get keyboard interrupt")
+		sys.exit(0)
+	except Exception:
+		logger.info("dingdang run error")
+		sys.exit(1)
+		
+		
